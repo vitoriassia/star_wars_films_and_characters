65235c763082b151ce5ea17ea044f89b184253ee
Vitor Ferreira Iassia <iassiavitor@gmail.com>
2021-09-09 12:14:44 -0300

    excluindo arquivos desnecessarios

--- lib/pages/root_page.dart
+++ lib/pages/root_page.dart
@@ -1,7 +1,4 @@
-import 'package:star_wars_films_and_characters/main.dart';
 import 'package:star_wars_films_and_characters/pages/home/home_module.dart';
-import 'package:star_wars_films_and_characters/pages/login/login_module.dart';
-import 'package:star_wars_films_and_characters/shared/auth/auth_bloc.dart';
 import 'package:flutter/material.dart';
 
 class RootPage extends StatefulWidget {
@@ -10,21 +7,13 @@ class RootPage extends StatefulWidget {
 }
 
 class _RootPageState extends State<RootPage> {
-  var authBloc;
   @override
   void initState() {
-    authBloc = AppModule.to.getBloc<AuthBloc>();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<bool>(
-      stream: authBloc.statusOut,
-      initialData: false,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        return snapshot.data ? HomeModule() : LoginModule();
-      },
-    );
+    return HomeModule();
   }
 }
